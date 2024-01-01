4a5883334c356eb06ec1c590df8c459f16048b2e
ineak <ineak@stud.ntnu.no>
2024-01-01 01:14:25 +0100

    lagt til routing for alle sider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
 import './App.css'
 import { Homepage } from './pages/Homepage'
+import { Projectpage } from './pages/Projectpage'
+import { Aboutmepage } from './pages/Aboutmepage'
+import { Contactpage } from './pages/Contactpage'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Homepage/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage/>}></Route>
+          <Route path="/project/:project" element={<Projectpage/>}></Route>
+          <Route path="omMeg" element={<Aboutmepage/>}></Route>
+          <Route path="siHei" element={<Contactpage/>}></Route>
+        </Routes>
+        
+      </Router>
     </>
   )
 }
