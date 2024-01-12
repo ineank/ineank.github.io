24dec2a64497e0825f6c1f5b0dcfc9b0cf45aea8
ineak <ineak@stud.ntnu.no>
2024-01-13 00:05:19 +0100

    projectpage in progress

--- src/pages/Projectpage.jsx
+++ src/pages/Projectpage.jsx
@@ -1,6 +1,7 @@
 import { Header } from '../components/Header';
 import projectsData from '../projects.json';
 import './projectpage.css';
+import UXfigure from '../assets/UXfigure.svg';
 
 export function Projectpage() {
     const selectedProject = projectsData.find((project) => project.project === "Xpedition");
@@ -13,23 +14,79 @@ export function Projectpage() {
     return(
         <>
             <Header />
-            <div>
+            <div className='headline'>
                 <h1>"{selectedProject.project}",</h1>
                 <h2>{selectedProject.semester}</h2>
-                <div>
-                    <h2>{selectedProject.summary}</h2>
+            </div>
+                <div className='intro'>
+                    <h2 className='index'>{selectedProject.summary}</h2>
                     <img className="image_top" src={selectedProject.image[1]}></img>
                 </div>
-            </div>
+            
             <div className="lists">
                 <div>
-                    <p>Min rolle</p>
+                    <p><b>Min rolle</b></p>
+                    {Object.keys(selectedProject.role).map(key => (
+                        <p key={key}>{selectedProject.role[key]}</p>
+                    ))}
+
                 </div>
                 <div>
-                    <p>Teknologi</p>
+                    <p><b>Teknologi</b></p>
+                    {Object.keys(selectedProject.technology).map(key => (
+                        <p key={key}>{selectedProject.technology[key]}</p>
+                    ))}
                 </div>
                 <div>
-                    <p>Leveranse</p>
+                    <p><b>Leveranse</b></p>
+                    {Object.keys(selectedProject.delivery).map(key => (
+                        <p key={key}>{selectedProject.delivery[key]}</p>
+                    ))}
+                </div>
+            </div>
+
+            <div className='image_group'>
+                <div>
+                    <img src={selectedProject.image[2]}></img>
+                    <img src={selectedProject.image[3]}></img>
+                </div>
+                <img src={selectedProject.image[4]}></img>
+            </div>
+
+            <div className='about_project'>
+                <div>
+                    <h2>Om prosjektet</h2>
+                    <p>{selectedProject.about}</p>
+                </div>
+                <img src={UXfigure}></img>
+            </div>
+
+            <div className='process_top'>
+                <div>
+                    <h2>Prosess</h2>
+                    <p>{selectedProject.process[1]}</p>
+                    <p>{selectedProject.process[2]}</p>
+                </div>
+                <img src={selectedProject.process_image[1]}></img>
+            </div>
+
+            <div className='process_bottom'>
+                <div className='process_left'>
+                    <img src={selectedProject.process_image[2]}></img>
+                    <img src={selectedProject.process_image[3]}></img>
+                </div>
+                <div>
+                    <p>{selectedProject.process[3]}</p>
+                    <p>{selectedProject.process[4]}</p>
+
+                </div>
+            </div>
+
+            <div className='learnings'>
+                <h2>Hva har jeg lært?</h2>
+                <div>
+                    <p>{selectedProject.learnings[1]}</p>
+                    <p>{selectedProject.learnings[2]}</p>
                 </div>
             </div>
         </>
